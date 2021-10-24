19bf76b19cdecef26b94eb4a85227b6a4213fb93
RONENBE <RONENBE@rnd-hub.com>
2021-10-24 08:46:22 +0300

    connected all activities to db

--- app/src/main/java/com/restart/myapplicationactivitytest/SecondFragment.java
+++ app/src/main/java/com/restart/myapplicationactivitytest/SecondFragment.java
@@ -42,14 +42,159 @@ public class SecondFragment extends Fragment {
         binding.btnImgIncar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String eventName = "inCar";
                 if (v.isSelected() == false) {
                     v.setSelected(true);
                     v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
-                    handler.writeStartEventToDb("inCar");
+                    handler.writeStartEventToDb(eventName);
                 } else {
                     v.setSelected(false);
                     v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
-                    handler.writeEndEventToDb("inCar");
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgClosedSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "closedSpace";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgCrowded.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "crowded";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgDriving.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "driving";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "media";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgResting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "layDown";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgSleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "sleeping";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.btnImgWalking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "walking";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.imgBtnDrug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "medication";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
+                }
+            }
+        });
+
+        binding.imgWakeUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String eventName = "wakeUp";
+                if (v.isSelected() == false) {
+                    v.setSelected(true);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.sign_in_separator_color));
+                    handler.writeStartEventToDb(eventName);
+                } else {
+                    v.setSelected(false);
+                    v.setBackgroundTintList(getResources().getColorStateList(R.color.white));
+                    handler.writeEndEventToDb(eventName);
                 }
             }
         });
